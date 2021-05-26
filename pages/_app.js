48747ce6a960b50d4b7da2837782f1eb9e4b68e9
ashutosh <kukreti.ashutosh@gmail.com>
2021-05-26 17:04:59 +0530

    Common Header for all the components.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { Fragment } from "react";
+import HeaderComponent from "../components/Header/HeaderComponent";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Fragment>
+        <HeaderComponent />
+        <Component {...pageProps} />
+      </Fragment>
+
+  )
 }
 
 export default MyApp
