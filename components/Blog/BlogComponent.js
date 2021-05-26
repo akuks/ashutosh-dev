c860cd16500ab13656516d31b7aebabe6f5dc16d
ashutosh <kukreti.ashutosh@gmail.com>
2021-05-26 17:06:32 +0530

    Create Blog component

---
+++ components/Blog/BlogComponent.js
@@ -0,0 +1,11 @@
+import BlogList from "./BlogList";
+
+function BlogComponent() {
+    return (
+        <div className="ml-4 mt-4 w-1/2 container mr-4">
+            <BlogList />
+        </div>
+    )
+}
+
+export default BlogComponent
