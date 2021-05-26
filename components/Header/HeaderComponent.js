aa6082498761ded244a05fa47919f64fb8ba84d2
ashutosh <kukreti.ashutosh@gmail.com>
2021-05-26 17:09:22 +0530

    Create Header component

---
+++ components/Header/HeaderComponent.js
@@ -0,0 +1,11 @@
+import NavigationComponent from "./NavigationComponent";
+function HeaderComponent() {
+    return (
+        <div>
+            <NavigationComponent />
+        </div>
+    )
+}
+
+
+export default HeaderComponent;
