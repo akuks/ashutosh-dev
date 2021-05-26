cf4498b721f534b80b1814751d4d1b75d1b19fb7
ashutosh <kukreti.ashutosh@gmail.com>
2021-05-26 17:03:53 +0530

    Update the theme for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      }
+    },
   },
   variants: {
     extend: {},
