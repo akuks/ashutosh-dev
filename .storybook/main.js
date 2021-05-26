804fdf5334d9b18dd3784cf517c93e897cd06377
ashutosh <kukreti.ashutosh@gmail.com>
2021-05-26 17:10:11 +0530

    Update main.js storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+    "@storybook/addon-postcss",
+    "storybook-css-modules-preset"
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+    });
+    return config
+  }
 }
