bacfecc297d6fafbea16368a2763e8078cbc4742
ashutosh <kukreti.ashutosh@gmail.com>
2021-05-31 23:17:21 +0530

    Remove console.log

--- components/Blog/BlogComponent.js
+++ components/Blog/BlogComponent.js
@@ -1,7 +1,7 @@
 import BlogList from "./BlogList";
 
 function BlogComponent(props) {
-    // console.log(props)
+    
     return (
         <div className="ml-4 mt-4 w-1/2 container mr-4">
             <BlogList Blogs={props.Blogs} />
