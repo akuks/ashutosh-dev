a135d20cb0069dbb39c78cad4ec047e21c84e773
ashutosh <kukreti.ashutosh@gmail.com>
2021-05-31 23:12:15 +0530

    Use the props from BlogComponent

--- components/Blog/BlogList.js
+++ components/Blog/BlogList.js
@@ -1,72 +1,16 @@
-function BlogList() {
+import Link  from "next/link";
+import IndividualBlogDetails from "./IndividualBlogDetails";
+
+function BlogList(props) {
+    const Blogs  = props.Blogs.sample_blogs
+
     return (
         <div className="ml-4">
-            <div className="border-b shadow rounded-md hover:shadow-lg hover:border">
-                <div className="ml-4 mt-4">
-                    <h1 className="font-bold text-2xl">It's All About (The) Next.js api routes</h1>
-                </div>
-                {/*Posted Date*/}
-                <div className="ml-4 mt-4 flex">
-                    <p>
-                        <span className="text-gray-400">Posted On </span>
-                        <small className="hover:underline hover:text-cyan-500">May 22, 2021</small>
-                    </p>
-                    {/*Categories Belongs to*/}
-                    <div className="ml-10">
-                        <span className="inline-block bg-cyan-200 text-cyan-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide mr-2">
-                            Perl
-                        </span>
-                        <span className="inline-block bg-cyan-200 text-cyan-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide mb-10">
-                            Nextjs
-                        </span>
-                    </div>
-                </div>
-            </div>
-
-            <div className="border-b shadow rounded-md hover:shadow-lg hover:border">
-                <div className="ml-4 mt-4">
-                    <h1 className="font-bold text-2xl">Develop a blog in Next.js and Mojolicious - (Part - 1)</h1>
-                </div>
-                {/*Posted Date*/}
-                <div className="ml-4 mt-4 flex">
-                    <p>
-                        <span className="text-gray-400">Posted On </span>
-                        <small className="hover:underline hover:text-cyan-500">May 22, 2021</small>
-                    </p>
-                    {/*Categories Belongs to*/}
-                    <div className="ml-10">
-                        <span className="inline-block bg-cyan-200 text-cyan-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide mr-2">
-                            Perl
-                        </span>
-                        <span className="inline-block bg-cyan-200 text-cyan-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide mb-10">
-                            Nextjs
-                        </span>
-                    </div>
-                </div>
-            </div>
-
-            <div className="border-b shadow rounded-md hover:shadow-lg hover:border">
-                <div className="ml-4 mt-4">
-                    <h1 className="font-bold text-2xl">It's All About (The) Next.js Api routes (Part-2)</h1>
-                </div>
-                {/*Posted Date*/}
-                <div className="ml-4 mt-4 flex">
-                    <p>
-                        <span className="text-gray-400">Posted On </span>
-                        <small className="hover:underline hover:text-cyan-500">May 22, 2021</small>
-                    </p>
-                    {/*Categories Belongs to*/}
-                    <div className="ml-10">
-                        <span className="inline-block bg-cyan-200 text-cyan-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide mr-2">
-                            Perl
-                        </span>
-                        <span className="inline-block bg-cyan-200 text-cyan-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide mb-10">
-                            Nextjs
-                        </span>
-                    </div>
-                </div>
-            </div>
-
+            {
+                Blogs.map( event =>
+                    <IndividualBlogDetails details={event} />
+                )
+            }
         </div>
     )
 }
