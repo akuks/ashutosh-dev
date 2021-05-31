ad19c009888856db0ae39badc531675218aab231
ashutosh <kukreti.ashutosh@gmail.com>
2021-05-31 06:11:42 +0530

    Replace anchor tag with Link

--- components/Header/NavigationComponent.js
+++ components/Header/NavigationComponent.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 function NavigationComponent() {
     return (
         <div className="bg-gradient-to-r from-cyan-400 to-light-blue-500 rounded shadow-lg">
+
             <nav className="flex flex-wrap items-center justify-between p-4  rounded">
+
                 <div className="block lg:hidden">
                     <button
                         className="navbar-burger flex items-center py-2 px-3 text-indigo-500 rounded border border-indigo-500">
@@ -11,23 +15,33 @@ function NavigationComponent() {
                         </svg>
                     </button>
                 </div>
+
                 <div className="navbar-menu hidden lg:flex lg:flex-grow lg:items-center w-full lg:w-auto">
                     <div className="lg:mx-auto">
-                        <a className="block lg:inline-block mt-4 lg:mt-0 mr-10 font-bold text-white hover:underline"
-                        href="/">
-                            Home
-                        </a>
-                        <a className="block lg:inline-block mt-4 lg:mt-0 mr-10 font-bold text-white hover:underline"
-                        href="/about">
-                            About
-                        </a>
-                        <a className="block lg:inline-block mt-4 lg:mt-0 text-white font-bold hover:underline"
-                        href="/contact">
-                            Contact
-                        </a>
+                        <Link href="/">
+                            <a className="block lg:inline-block mt-4 lg:mt-0 mr-10 font-bold text-white hover:underline">
+                                Home
+                            </a>
+                        </Link>
+
+                        <Link href="/about">
+                            <a className="block lg:inline-block mt-4 lg:mt-0 mr-10 font-bold text-white hover:underline">
+                                About
+                            </a>
+                        </Link>
+
+                        <Link  href="/contact">
+                            <a className="block lg:inline-block mt-4 lg:mt-0 text-white font-bold hover:underline">
+                                Contact
+                            </a>
+                        </Link>
+
                     </div>
+
                 </div>
+
             </nav>
+
         </div>
     )
 }
