dfb43c9a34a608ed45102a814ff73fd71f64dfbf
ashutosh <kukreti.ashutosh@gmail.com>
2021-06-01 20:30:40 +0530

    Pass title prop to Blog Details

--- components/Blog/BlogList.js
+++ components/Blog/BlogList.js
@@ -5,10 +5,10 @@ function BlogList(props) {
     const Blogs  = props.Blogs.sample_blogs
 
     return (
-        <div className="ml-4">
+        <div className="ml-4" >
             {
                 Blogs.map( event =>
-                    <IndividualBlogDetails details={event} />
+                    <IndividualBlogDetails key={event.title} details={event} />
                 )
             }
         </div>
