46cca77dd20e8475340fe9332a402437aa16ae14
ashutosh <kukreti.ashutosh@gmail.com>
2021-06-01 20:30:55 +0530

    Fix the key error in Nextjs

--- components/Blog/IndividualBlogDetails.js
+++ components/Blog/IndividualBlogDetails.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 function IndividualBlogDetails(props) {
     const event = props.details
     const cat = (event.categories).split(", ");
-
+    let count = 1;
     return (
         <div key={event.slug} className={`border-b shadow rounded-md hover:shadow-lg hover:border`}>
             <div className={`ml-4 mt-4`}>
@@ -27,7 +27,7 @@ function IndividualBlogDetails(props) {
                     {
                         cat.map
                         ( event =>
-                            <span key={cat} className={`inline-block bg-cyan-200 text-cyan-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide mr-2`}>
+                            <span key={cat + count++} className={`inline-block bg-cyan-200 text-cyan-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide mr-2`}>
                                 <Link href={`/?categories=${encodeURIComponent(event)}` }>
                                     <a className={`hover:underline`}>{event}</a>
                                 </Link>
