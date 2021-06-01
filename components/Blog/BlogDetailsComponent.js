30f91d10431ce0c9a4d82c5f7b32fc939247d577
ashutosh <kukreti.ashutosh@gmail.com>
2021-06-01 20:30:04 +0530

    Update BlogDetails Component

---
+++ components/Blog/BlogDetailsComponent.js
@@ -0,0 +1,10 @@
+function BlogDetailsComponent() {
+    const title  = 'Title of the post';
+    return (
+        <div>
+            <h1> {title} </h1>
+        </div>
+    )
+} 
+
+export default BlogDetailsComponent
