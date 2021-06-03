73766cd77419762fa8a7c3bbee8409fd0f9e3360
ashutosh <kukreti.ashutosh@gmail.com>
2021-06-03 21:03:54 +0530

    To get the Individual code

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,11 +1,35 @@
 import BlogDetailsComponent from "../../components/Blog/BlogDetailsComponent";
+import {getBlogData} from "../../Utilities/FileRead";
+import CategoriesComponent from "../../components/Categories/CategoriesComponent";
+import BlogComponent from "../../components/Blog/BlogComponent";
 
 function IndividualBlogPage(props) {
+
     return (
-        <div>
-            <BlogDetailsComponent />
+        <div className="flex flex-auto">
+            <CategoriesComponent />
+            <BlogDetailsComponent blog={props}/>
+            <div></div>
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    const fileName = '/data/articles.json';
+    const { params } = context
+
+    const blogData = getBlogData(fileName, params.slug)
+
+    return {
+        props:{
+            title: blogData.title,
+            date: blogData.published_at,
+            body: blogData.body_markdown,
+            description: blogData.description,
+            categories: blogData.cached_tag_list,
+            slug: params.slug
+        }
+    }
 }
 
 export default IndividualBlogPage
