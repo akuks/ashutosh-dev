8b5fd8b0cc53f4f5cc0530c3dcdfd5465c77e6e3
ashutosh <kukreti.ashutosh@gmail.com>
2021-06-08 00:20:54 +0530

    Fix the broken file code

--- components/Blog/BlogDetailsComponent.js
+++ components/Blog/BlogDetailsComponent.js
@@ -5,9 +5,15 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 function BlogDetailsComponent(props) {
-    // let body = props.blog.body
-    // let res = body.match(/<img.* \/>/g);
-    // console.log(res)
+    let body = props.blog.body
+    let res = body.match(/<img.* \/>/g);
+
+    for (let r in res) {
+        let imageInfo = res[r].split("\"")
+        let temp = body.replace(res[r], '![Image ' + r + '](' + imageInfo[1] + ')' )
+        body = temp
+    }
+
     const customComponents = {
 
         code(code) {
@@ -23,15 +29,11 @@ function BlogDetailsComponent(props) {
                 <SyntaxHighlighter language={language[language.length -1]} children={children} style={atomDark}   />
             )
         },
-        paragraph(p) {
-            console.log(p)
-            return (p)
-        },
     }
 
     return (
         <div className={`ml-10 mt-4 w-3/5 container mr-4 shadow rounded border blogs `}>
-            <ReactMarkdown components={customComponents}>{props.blog.body}</ReactMarkdown>
+            <ReactMarkdown components={customComponents}>{body}</ReactMarkdown>
             <style jsx global>
                 {styles}
             </style>
