18fa0309d8993cda1474cf737d752936c9a783e6
ashutosh <kukreti.ashutosh@gmail.com>
2021-06-09 00:57:29 +0530

    Change the Url for categories

---
+++ pages/categories/[category].js
@@ -0,0 +1,47 @@
+import {getCategoryBlogsData} from "../../Utilities/FileRead";
+import CategoriesComponent from "../../components/Categories/CategoriesComponent";
+import BlogComponent from "../../components/Blog/BlogComponent";
+import CategoriesList from "../../components/Categories/CategoriesList";
+
+function Category(props) {
+    // const category = props.category;
+    // const data = props.data
+
+    // console.log(props.category)
+    return (
+        <div className="flex flex-auto">
+            <CategoriesComponent />
+            <BlogComponent Blogs={props} />
+            <div></div>
+        </div>
+    )
+}
+
+export async function getStaticProps(context) {
+    const { category } = context.params
+
+    const data = getCategoryBlogsData('/data/articles.json', category)
+
+    return  {
+        props: {
+            sample_blogs: data,
+            category: category
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const categoriesList = CategoriesList
+    return {
+        paths: [
+            {  params: { category: 'perl' } },
+            {  params: { category: 'nextjs' } },
+            {  params: { category: 'python' } },
+            {  params: { category: 'dataanalysis' } },
+            {  params: { category: 'mojolicious' } },
+        ],
+        fallback: true
+    }
+}
+
+export default Category
